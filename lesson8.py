da28ee2fb84af17138e007804b3ab1923969a25e
collan sang <collankimtaisang@gmail.com>
2025-02-01 21:49:21 +0300

    This is my third commit

--- lesson8.py
+++ lesson8.py
@@ -1,14 +1,14 @@
-#temperature converter from celsiuu\s to pounds and visw versa
-temp = float(input("Please enter your temperature here: "))
-unit = input("Is your temperature in celsius or fahrenheit(C, F)")
-
-if unit == "C":
-    temp = round(((9 * temp) / 5) + 32, 2)
-    print(f"Your temperature is {temp} fahrenheit.")
-
-elif unit == "F":
-    temp = round((5/9) * (temp - 32), 2)
-    print(f" Your temperature is {temp} celsius")
-
-else:
-    print("The input you entered is not recognized. please try again.")
+# #temperature converter from celsiuu\s to pounds and visw versa
+# temp = float(input("Please enter your temperature here: "))
+# unit = input("Is your temperature in celsius or fahrenheit(C, F)")
+#
+# if unit == "C":
+#     temp = round(((9 * temp) / 5) + 32, 2)
+#     print(f"Your temperature is {temp} fahrenheit.")
+#
+# elif unit == "F":
+#     temp = round((5/9) * (temp - 32), 2)
+#     print(f" Your temperature is {temp} celsius")
+#
+# else:
+#     print("The input you entered is not recognized. please try again.")
--- lesson9.py
+++ lesson9.py
@@ -1,1 +1,31 @@
-#logical operates : and Returns True if both conditions are true, Otherwise it Returns False.
+#logical operators
+#and: checks if both statements are true and returns True
+#or: if one of he statements is true it returns True
+#not : reverses the results
+
+
+age = float(input("How old are you?: "))
+weight = float(input("What is your weight in KGs?: "))
+HIV_status = input("Are you HIV positive/negative(p/n)")
+
+if age >= 18 and weight >=50 and HIV_status == "n":
+    print("You meet all the qualifications to donate blood, "
+          "Please donate at the nearest health facility to help a soul")
+
+elif age >= 18 and weight <50 and HIV_status == "n":
+    print("Your weight is too low, you dont qualify for a blood donation. Welcome back next time.")
+
+elif 18> age >= 0 and weight >= 50 and HIV_status == "n":
+    print("You should be 18+ years too donate your blood.")
+
+elif age >= 18 and weight >= 50 and HIV_status =="p":
+    print("You do not qualify for blood donations due to health issues, "
+          "please conduct a certified medical practitioner for more assistance ")
+
+elif age < 18 and weight < 50 or HIV_status == "p":
+    print("You dont qualify for blood donations")
+
+
+
+else:
+    print("Please check your answers and enter them correctly")
