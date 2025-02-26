accde05d15a3c12df4f14246f5905250f40b1408
collan sang <collankimtaisang@gmail.com>
2025-02-26 18:09:05 +0300

    Dictionaries

---
+++ lesson25.py
@@ -0,0 +1,21 @@
+#dictionary= is a collection of keys and values ordered and changable.. key: value
+
+capitals = {'kenya':'Nairobi',
+            'Uganda ':'Kampala',
+            'Tanzania':'Dares-salaam',
+            'Ethiopia':'Khartoum'}
+#print(capitals.get('kenya')) #will get the value of the key you entered and if its not present it prints none
+#capitals.update({'Germany':'berlin'})# will update your dictionary or insert
+#print(help(capitals))# will print all things you can do with your dictionary. You can use anywhere with the same format
+
+#capitals.pop('kenya')# will remove an item
+#capitals.popitem() #will remove the latest key value
+#capitals.clear()# will clear the dictionary
+
+#keys = capitals.keys() # will print all keys in your dictionary in this case countries
+#for key in keys:#will iterate your string in single ine each
+    #print (key)
+
+#values = capitals.values() # will print all values(capital cities)
+#for value in values:
+    #print (value)
